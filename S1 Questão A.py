d8be5d654f0cb924719fb73f1cc230776dbbcb36
Behael <160921614+Behaelson@users.noreply.github.com>
2024-05-13 20:44:32 +0000

    Semana 01

--- S1 Questão A.py
+++ S1 Questão A.py
@@ -1,42 +1,47 @@
 import random
-import time
+import time 
 
-a = "Pedra"
-b = "Papel"
-c = "Tesoura"
-
-# a > b > c and c > a
-
-Escolhas = (a, b, c)
+Escolhas = ("pedra", "papel", "tesoura")
 tentativas = 0
 vitorias = 0
+derrotas = 0
 
 PC = random.choice(Escolhas)
 
 while tentativas < 5:
-    if vitorias == 3:
-        print("LEsgoooo")
+    if vitorias >= 3:
+        print("Parabéns! Você ganhou!! :D")
         break
-    print("Pedra, Papel ou Tesoura?"), '\n'
-    P1 = input()  
+    if derrotas >= 3:
+        print("Que pena! Você perdeu! :(")
+        break
+    print('\n', "Pedra, Papel ou Tesoura?"), '\n'
+    P1 = str.lower(input("Escolha do Jogador: "))
     if P1 in Escolhas: 
-        # print("Jô")
-        # time.sleep(0.5)
-        # print("Ken")
-        # time.sleep(0.5)
-        # print("Pô!")
-        # time.sleep(1.0) 
-        print(PC)
-        # PCind = Escolhas.index(PC)
-        # P1ind = Escolhas.index(P1)
+        print("Jô")
+        time.sleep(0.5)
+        print("Ken")
+        time.sleep(0.5)
+        print("Pô!")
+        time.sleep(1.0) 
+        print("Escolha do adversário:", PC)
+        PCind = Escolhas.index(PC)
+        P1ind = Escolhas.index(P1)
+        PX = P1ind - PCind
         tentativas = tentativas + 1
-        if P1 > PC:
-           print("Perdeu!") 
-        elif P1 == PC:
-            print("Empate")
+        if PX == 1 or PX == -2:
+           print("Ganhou!") 
+           vitorias = vitorias + 1
+        elif PX == 0:
+            print("Empate!")
         else:
-            print("Ganhou!")
-            vitorias = vitorias + 1
-        
+            print("Perdeu!")
+            derrotas = derrotas + 1
 
-print("CABO")
+final = vitorias - derrotas
+if final > 0:
+    print("Parabéns, você ganhou por pouco!")
+elif final == 0:
+    print("É... Empatou de vez!")
+else:
+    print("Mesmo sem perder 3 vezes... você perdeu no final... ;-;")
