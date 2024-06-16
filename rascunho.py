bb792b801d00311f8a1619f6bee7c670d53351d5
Behael <160921614+Behaelson@users.noreply.github.com>
2024-06-16 23:17:11 +0000

    rascunho

--- rascunho.py
+++ rascunho.py
@@ -1,2 +1,19 @@
-X = "batata"
-print(list(X))
+i = 0
+j = 1
+while i <= 9:
+    i += 3
+    j += 2
+    if i == j:
+        print("-")
+    else:
+        print(".")
+
+while j < 15:
+    j += 1.5
+    if j == i:
+        print(".")
+    else:
+        print("-")
+
+print(".")
+
